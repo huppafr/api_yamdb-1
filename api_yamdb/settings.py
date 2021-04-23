8d2b0b30f56e309a986f5362dd5146d62c8fa042
LenaKiseleva <ellen91981@mail.ru>
2021-04-23 11:16:49 +0300

    Delete square brackets

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -130,9 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_PAGINATION_CLASS': [
-        'rest_framework.pagination.LimitOffsetPagination',
-    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
