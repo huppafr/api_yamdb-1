101615d57351aceb7da8aaadc2007cce113f6e4e
LenaKiseleva <ellen91981@mail.ru>
2021-05-05 15:41:41 +0300

    Commit

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -10,7 +9,11 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .generate_code import generate_confirmation_code, send_mail_to_user
 from .models import User
 from .permissions import IsAdmin, IsSuperuser
-from .serializers import UserSerializer, EmailSerializer
+from .serializers import (
+    UserSerializer,
+    EmailSerializer,
+    CodeSerializer,
+)
 
 
 class RegisterView(APIView):
@@ -43,17 +46,21 @@ class RegisterView(APIView):
 class TokenView(APIView):
     permission_classes = (AllowAny,)
     serializer = UserSerializer
+    email_serializer = EmailSerializer
+    code_serializer = CodeSerializer
 
     def get_token(self, user):
         refresh = RefreshToken.for_user(user)
         return str(refresh.access_token)
 
     def post(self, request):
-        validate = self.serializer(data=request.data)
-        validate.is_valid(raise_exception=True)
-        email = validate.validated_data('email')
-        user = get_object_or_404(User, email=email)
-        code = validate.validated_data('confirmation_code')
+        validate_email = self.email_serializer(data=request.data)
+        validate_email.is_valid(raise_exception=True)
+        validate_code = self.code_serializer(data=request.data)
+        validate_code.is_valid(raise_exception=True)
+        email = validate_email.validated_data['email']
+        code = validate_code.validated_data['confirmation_code']
+        user = User.objects.filter(email=email).first()
         if user.confirmation_code != code:
             response = {'confirmation_code': 'Неверный код'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
